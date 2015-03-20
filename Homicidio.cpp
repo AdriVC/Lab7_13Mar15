278868e3b35e5fd520da132ef178322c6c85f64b
Ambar Suarez <ambarsuarez@unitec.edu>
2015-03-20 14:26:58 -0600

    modificar Homicidio.cpp

--- Homicidio.cpp
+++ Homicidio.cpp
@@ -7,8 +7,6 @@
 
 Homicidio::Homicidio(string sospechoso_p, string culpable, string victima, int num_caso, string hour, string date, bool closed)
 :Casos(num_caso,hour,date,closed), sospechoso_p(sospechoso_p), culpable(culpable), victima(victima){
-	vector<Investigator*> investigators;
-	vector<Evidencias*> evidence;
 	vector<string> sospechosos;
 }
 
